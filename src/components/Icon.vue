877806033b0156baff342e2cc0e8568a6fc7500f
董立石 <173110115@qq.com>
2025-08-24 15:59:41 +0800

    release: v0.3.10

---
+++ src/components/Icon.vue
@@ -0,0 +1,38 @@
+<template>
+  <svg :class="classes.icon" aria-hidden="true" :style="style">
+    <use :xlink:href="'#' + icon"></use>
+  </svg>
+</template>
+
+<script lang="ts" setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  icon: {
+    type: String,
+    required: true,
+  },
+  size: {
+    type: String,
+    default: '14px',
+  },
+  color: {
+    type: String,
+  },
+})
+
+const style = computed(() => {
+  return {
+    fontSize: props.size,
+    color: props.color,
+  }
+})
+</script>
+
+<style module="classes">
+.icon {
+  width: 1em;
+  height: 1em;
+  fill: currentColor;
+}
+</style>
--- src/components/utils.tsx
+++ src/components/utils.tsx
@@ -1,4 +1,6 @@
 import classes from '../styles/window.module.css'
+import Icon from './Icon.vue'
+
 import type { BlankWindow } from '../model/Windows'
 
 import { IconWindow } from '../svg/index'
@@ -7,7 +9,11 @@ import { isVNode } from 'vue'
 export function createIcon(win: BlankWindow): any {
   const icon = win.options.icon
 
-  if (typeof icon == 'string') return <i class={[classes.logo, 'icon', icon]} />
+  if (typeof icon == 'string') {
+    if (icon.startsWith('svg:')) return <Icon icon={icon.slice(4)} size="18px" />
+    return <i class={[classes.logo, 'icon', icon]} />
+  }
+
   if (isVNode(icon)) return icon
   if (typeof icon == 'function') return icon(win)
 
